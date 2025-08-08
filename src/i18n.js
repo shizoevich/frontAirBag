e31cb78594f40c6b016633a0f73cda6d6597a0cc
elizabeth <elizabeth.pysarenko@gmail.com>
2025-08-08 17:25:46 +0300

    search fix

--- src/i18n.js
+++ src/i18n.js
@@ -8,11 +8,14 @@ export default getRequestConfig(async ({locale}) => {
   // Validate that the incoming `locale` parameter is valid
   if (!locales.includes(locale)) {
     return {
+      locale: 'en',
       messages: (await import(`../messages/en.json`)).default
     };
   }
 
   return {
+    locale,
     messages: (await import(`../messages/${locale}.json`)).default
   };
+
 });
